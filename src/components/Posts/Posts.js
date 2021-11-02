f3cd267c8ec1ccf4ef39f6d54788558ea6ffd390
richad.97 <rcastillo2022@gmail.com>
2021-11-02 16:14:03 -0700

    assignment nearly complete/LikeSection/likePost callback need work

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import Post from './Post';
-import './Posts.css';
+import React from "react";
+import Post from "./Post";
+import "./Posts.css";
 
 const Posts = (props) => {
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
   return (
-    <div className='posts-container-wrapper'>
+    <div className="posts-container-wrapper">
       {/* Map through the posts array returning a Post component at each iteration */}
+
+      {posts.map((obj, ind) => {
+        return <Post post={obj} likePost={likePost} key={ind}></Post>;
+      })}
+
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
